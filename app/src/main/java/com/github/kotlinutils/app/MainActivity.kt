4ac12c64ee5351b9787111536d6d055ae764f352
Borislav Nesterov <borislav.nesterov777@gmail.com>
2017-07-26 16:21:38 +0300

    Clean-up project from unused files
    - delete auto-generated test examples
    
    build.gradle files
    - set lib version to "0.1.0"
    - move plugin and lib versions to Project's build.gradle

---
+++ app/src/main/java/com/github/kotlinutils/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.kotlinutils.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.kotlinutils.R
+
+/**
+ * Created by nbv54 on 26-Jul-17.
+ */
+class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
